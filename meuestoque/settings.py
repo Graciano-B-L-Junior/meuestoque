b20e361899ef5e77d78ed822f3d671e512e2b8c6
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2025-06-12 22:27:44 -0300

    Pequeno mvp finalizado

--- meuestoque/settings.py
+++ meuestoque/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1a$gr==a3-$^%9k6_7^aiwxp#u709-rlqj(r%derdcz!#zb^1@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
