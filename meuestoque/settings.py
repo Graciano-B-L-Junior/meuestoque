42dbd570cc4d01f84fc0a31afb7242643eedfcb4
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2025-06-15 18:17:16 -0300

    atualização para deploy

--- meuestoque/settings.py
+++ meuestoque/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1a$gr==a3-$^%9k6_7^aiwxp#u709-rlqj(r%derdcz!#zb^1@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
